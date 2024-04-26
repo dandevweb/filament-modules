13ec70b1f0385d3f294f2a0e496afbd365bff84e
Danilo Augusto <dandevweb@gmail.com>
2024-04-26 07:05:27 -0300

    configuring translations with filament

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -3,38 +3,53 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    public static function getModelLabel(): string
+    {
+        return __('User');
+    }
+
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 Forms\Components\TextInput::make('name')
+
                     ->required()
                     ->maxLength(255),
                 Forms\Components\TextInput::make('email')
+
                     ->email()
                     ->required()
+                    ->unique(ignoreRecord: true)
                     ->maxLength(255),
-                Forms\Components\DateTimePicker::make('email_verified_at'),
+
                 Forms\Components\TextInput::make('password')
+
                     ->password()
-                    ->required()
+                    ->required(fn (string $operation): bool => $operation === 'create')
+                    ->confirmed()
+                    ->dehydrated(fn (?string $state) => filled($state))
                     ->maxLength(255),
+
+                Forms\Components\TextInput::make('password_confirmation')
+
+                    ->password()
+                    ->requiredWith('password')
+                    ->dehydrated(false)
+
+
             ]);
     }
 
@@ -43,17 +58,22 @@ class UserResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('name')
+
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email')
+
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email_verified_at')
+
                     ->dateTime()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('created_at')
+
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('updated_at')
+
                     ->dateTime()
                     ->sortable()
                     ->toggleable(isToggledHiddenByDefault: true),
